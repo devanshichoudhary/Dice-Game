7171f53110ae457a2d58a060cf69f917914b7f9c
Devanshi Choudhary <60618789+devanshichoudhary@users.noreply.github.com>
2021-08-20 19:02:02 +0530

    Dice game starting files

---
+++ index.js
@@ -0,0 +1,24 @@
+var randomNumber1=Math.floor(Math.random()*6)+1;
+var randomNumber2=Math.floor(Math.random()*6)+1;
+
+var randomImage1= "dice"+randomNumber1+".png";
+var imageSource1="images/"+randomImage1
+
+var randomImage2= "dice"+randomNumber2+".png";
+var imageSource2="images/"+randomImage2
+
+var image1=document.querySelectorAll("img")[0];
+image1.setAttribute("src",imageSource1);
+
+var image2=document.querySelectorAll("img")[1];
+image2.setAttribute("src",imageSource2);
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="Player 1 Wins!";
+}
+else if(randomNumber1===randomNumber2){
+    document.querySelector("h1").innerHTML="It's a draw!";   
+}
+else{
+    document.querySelector("h1").innerHTML="Player 2 Wins!";
+}
